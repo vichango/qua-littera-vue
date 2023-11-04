c02059e5ccd60213393252afc4f60fe1960d137e
Vichango <vichango@gmail.com>
2023-11-04 18:58:26 +0100

    Add QR code

--- src/components/interface/PlayerUi.vue
+++ src/components/interface/PlayerUi.vue
@@ -8,6 +8,7 @@
         <div class="w-full flex flex-col lg:flex-row h-screen">
           <div class="lg:basis-1/2">
             <YourLetters :event="props.event" :player-id="props.playerId" />
+            <qrcode-vue :value="url" size="128" level="L" />
           </div>
           <div class="lg:basis-1/2">
             <AllLetters :event="event" />
@@ -19,6 +20,8 @@
 </template>
 
 <script setup>
+import QrcodeVue from "qrcode.vue";
+import { computed } from "vue";
 import CaptureMain from "../capture/CaptureMain.vue";
 import AllLetters from "../lists/AllLetters.vue";
 import YourLetters from "../lists/YourLetters.vue";
@@ -26,6 +29,10 @@ import YourLetters from "../lists/YourLetters.vue";
 const props = defineProps({
   event: { type: Object, required: true },
   playerId: { type: String, required: true },
+});
+
+const url = computed(() => {
+  return `https://qua-littera.pruebitas.dev?eid=${props.event.id}&pid=${props.playerId};`;
 });
 </script>
 
