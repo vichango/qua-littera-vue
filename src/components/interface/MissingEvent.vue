d6718bf3917c54be42115c0504642709706c96ee
Vichango <vichango@gmail.com>
2023-11-06 22:58:19 +0100

    Add logo and favicon.

--- src/components/interface/MissingEvent.vue
+++ src/components/interface/MissingEvent.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="w-screen h-screen bg-gray-100 flex justify-center">
-    <div class="flex m-auto">
-      <h1 class="text-2xl">Morphogrape</h1>
+    <div class="flex flex-col m-auto">
+      <img src="/logo.svg" class="block" />
+      <h1 class="text-2xl mt-4">Morphogrape</h1>
     </div>
   </div>
 </template>
