48b7d3a7a9619ffc72bcfdfc47107f9b1034b370
Vichango <vichango@gmail.com>
2023-11-06 23:05:12 +0100

    Also share player QR code

--- src/components/interface/QrCodeUi.vue
+++ src/components/interface/QrCodeUi.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="w-screen h-screen bg-gray-100 flex justify-center">
     <div class="flex m-auto">
-      <div v-if="player">
-        <qrcode-vue :size="200" :value="playerUrl" level="L" class="m-3" />
+      <div v-if="player" class="flex flex-col items-center">
+        <qrcode-vue
+          :size="200"
+          :value="playerUrl"
+          level="L"
+          class="m-3"
+          @click="player = !player"
+        />
+        <p class="text-center text-xl mt-4">
+          Partager l'événement avec le joueur
+        </p>
       </div>
-      <div v-else>
-        <qrcode-vue :value="eventUrl" :size="200" level="L" class="m-3" />
-        <p class="text-center text-xl">Partager l'événement</p>
+      <div v-else class="flex flex-col items-center">
+        <qrcode-vue
+          :value="eventUrl"
+          :size="200"
+          level="L"
+          class="m-3"
+          @click="player = !player"
+        />
+        <p class="text-center text-xl mt-4">Partager l'événement</p>
       </div>
     </div>
   </div>
