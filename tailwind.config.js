77c5694e2db1178c9d35f6636a9a9f286ec5eed8
Vichango <vichango@gmail.com>
2023-11-12 19:08:38 +0100

    Use actually computed classes in whitelist.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 const colors = ['blue', 'green']
 const whitelist = []
 colors.map((x) => {
-  whitelist.push(`color-${x}-200`)
-  whitelist.push(`color-${x}-300`)
-  whitelist.push(`color-${x}-400`)
-  whitelist.push(`color-${x}-500`)
+  whitelist.push(`border-${x}-500`)
+  whitelist.push(`text-${x}-500`)
+  whitelist.push(`bg-${x}-200`)
+  whitelist.push(`bg-${x}-300`)
+  whitelist.push(`bg-${x}-400`)
 })
 
 module.exports = {
