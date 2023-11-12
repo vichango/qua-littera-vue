0eaf85495cc593ff245118bf9873de5107912602
Vichango <vichango@gmail.com>
2023-11-12 19:03:11 +0100

    Tailwind purge color whitelist.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,19 @@
+const colors = ['blue', 'green']
+const whitelist = []
+colors.map((x) => {
+  whitelist.push(`color-${x}-200`)
+  whitelist.push(`color-${x}-300`)
+  whitelist.push(`color-${x}-400`)
+  whitelist.push(`color-${x}-500`)
+})
+
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: {
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    options: {
+      whitelist,
+    },
+  },
   darkMode: false,
   theme: {
     extend: {},
